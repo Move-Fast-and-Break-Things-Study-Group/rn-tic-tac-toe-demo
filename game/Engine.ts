661043254f6632c7738757f2beaeb80424ad28be
Yurij Mikhalevich <yurij@mikhalevi.ch>
2022-04-30 22:08:19 +0400

    feature: add makeMove and tests

--- game/Engine.ts
+++ game/Engine.ts
@@ -9,6 +9,11 @@
 // - получать информацию об ошибке/о конце игры/победе/поражении
 // - ошибки: попытка сделать запрещённый ход (можем пока ошибки не делать)н
 
+// Движок
+// а) уведомлять игрока о том, что наступила его очередь ходить
+// б) позволять сделать ход
+// в) предоставлять игроку доступ к состоянию игры (к игровой «доске»)
+
 /**
  * [
  *  [O, X, O],
@@ -31,8 +36,8 @@ type OnMoveFn = (state: State, makeMove: MakeMoveFn) => void;
 
 export default class Engine {
   private state: State;
-  private onPlayerOneMove: OnMoveFn;
-  private onPlayerTwoMove: OnMoveFn;
+  private onPlayerXMove: OnMoveFn;
+  private onPlayerOMove: OnMoveFn;
 
   constructor(onPlayerOneMove: OnMoveFn, onPlayerTwoMove: OnMoveFn) {
     this.state = [
@@ -41,11 +46,32 @@ export default class Engine {
       [Cell.Empty, Cell.Empty, Cell.Empty],
     ];
 
-    this.onPlayerOneMove = onPlayerOneMove;
-    this.onPlayerTwoMove = onPlayerTwoMove;
+    this.onPlayerXMove = onPlayerOneMove;
+    this.onPlayerOMove = onPlayerTwoMove;
   }
 
   getState(): State {
     return this.state;
+  }
+
+  startGame() {
+    const playerXMakeMove = (x: number, y: number) => this.makeMove(Cell.X, x, y);
+    this.onPlayerXMove(this.state, playerXMakeMove);
+  }
+
+  private makeMove(type: Cell.X | Cell.O, x: number, y: number) {
+    if (this.state[x][y] !== Cell.Empty) {
+      throw new Error('Нельзя сделать ход в уже занятую клетку');
+    }
+
+    this.state[x][y] = type;
+
+    if (type === Cell.X) {
+      const playerOMakeMove = (x: number, y: number) => this.makeMove(Cell.O, x, y);
+      this.onPlayerOMove(this.state, playerOMakeMove);
+    } else {
+      const playerXMakeMove = (x: number, y: number) => this.makeMove(Cell.X, x, y);
+      this.onPlayerXMove(this.state, playerXMakeMove);
+    }
   }
 }
